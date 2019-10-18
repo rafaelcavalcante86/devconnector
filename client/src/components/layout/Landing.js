b67d9967e71fb1a90d12f26d43292a9b51982c3b
Rafael Cavalcante <rafaelc@clickandgo.com>
2019-10-18 13:40:42 +0100

    Redux setup and alerts on going...

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
     return (
-        <section class="landing">
+        <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
           <h1 className="x-large">Developer Connector</h1>
